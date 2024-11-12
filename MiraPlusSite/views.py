161c8d3a9710bde0347d0b4d36b137a8420437c6
Qvverty72 <car.sotoa@duocuc.cl>
2024-11-12 12:45:49 -0300

    Added debt view and filters

--- MiraPlusSite/views.py
+++ MiraPlusSite/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Deuda
 
-# Create your views here.
+def deuda_list(request):
+    # Fetch all Deuda records
+    deudas = Deuda.objects.all()
+
+    # Optional: Filter by estado if query parameter is provided
+    estado = request.GET.get('estado')
+    if estado:
+        deudas = deudas.filter(estado=estado)
+
+    # Pass deudas to template
+    return render(request, 'deudas.html', {'deudas': deudas})
+
+
